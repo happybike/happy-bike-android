81690f98b566673897b588f9c67265bb7bd91541
Mihai Balint <balint.mihai@gmail.com>
2017-05-26 23:57:27 +0300

    hide title bar, crop image

--- mobile/src/main/java/space/velociraptors/happybike/WalkThrough.java
+++ mobile/src/main/java/space/velociraptors/happybike/WalkThrough.java
@@ -6,7 +6,6 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class WalkThrough extends AppCompatActivity {
 
